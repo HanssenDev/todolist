d6be09008e8d272fa1e595c5eae27e1618bfd3aa
Hanssen <hramanjooloo@gmail.com>
2020-07-26 23:54:59 +0100

    add ejs layouts

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 let items = [];
+let workItems = [];
 
 // Use EJS
 app.set("view engine", "ejs");
@@ -24,19 +25,25 @@ app.get("/", function(req, res) {
 
     let day = today.toLocaleDateString("en-UK", options);
 
-    res.render("list", {whichDay: day, newListItems: items})
+    res.render("list", {listTitle: day, newListItems: items})
 })
 
 
 app.post("/", function(req, res) {
     let item = req.body.newItem;
-    
-    items.push(item);
 
-    res.redirect("/");
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 })
-
-
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
