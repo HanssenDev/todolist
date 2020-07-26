042653ca85d0993f807f6601310f1fd731ca9781
Hanssen <hramanjooloo@gmail.com>
2020-07-26 19:50:24 +0100

    create logic for todo list

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+let items = [];
+
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
+
+    let today = new Date();
+
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-UK", options);
+
+    res.render("list", {whichDay: day, newListItems: items})
 })
 
+
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+    
+    items.push(item);
+
+    res.redirect("/");
+})
 
 
 
